976c14bc209ce78340a45f6704fe681241024cd7
Andrey <cheyzercheyzer@yandex.ru>
2020-04-06 20:52:18 +0300

    Added Strategy and Observer

---
+++ Strategy/Filter/HighContrastFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    class HighContrastFilter : IFilter
+    {
+        public void Apply(string fileName)
+        {
+            Console.WriteLine("Applying high contrast filter");
+        }
+    }
+}
